7ccd9cf3c0f6ca910c7432f5dabd05b2a69dd2bd
Mathieu Trancoso <Trancoso.mathieu@gmail.com>
2022-12-30 21:46:53 +0100

    add associations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pokemon
+
+  scope :pending, -> { where(status: "pending") }
+  scope :accepted, -> { where(status: "accepted") }
+  scope :declined, -> { where(status: "declined") }
+  scope :canceled, -> { where(status: "canceled") }
+
 end
