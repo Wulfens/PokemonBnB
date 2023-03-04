e40723bd5bcba44c77946395fa6a09b289b99249
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2023-03-04 10:38:45 +0100

    preview image

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem "jbuilder"
 
 gem "devise"
 gem "pundit"
+gem "cloudinary"
 
 
 # Use Redis adapter to run Action Cable in production
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -5,6 +5,7 @@ class Pokemon < ApplicationRecord
 
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
 
   validates :name, :types, presence: true
 
